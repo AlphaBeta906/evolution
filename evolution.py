9380e65d2dbca6150e30f0645cdb96cfb35019c3
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-06-24 20:43:08 +0800

    Add files via upload

---
+++ evolution.py
@@ -0,0 +1,29 @@
+import random as r
+import time as t
+chance = 0
+creatures = {}
+# Template for creature: name: [type(plants, micrabes, animals), genes]
+genes = ["Small", "Big", "Energy Efficient", "ATP User", "Glucose User"]
+while True:
+    if len(creatures) == 0:
+        creatures["Creature 1"] = ["Microbe", "Small"]
+    else:
+        chance = r.randint(1, 10)
+        if chance == 1:
+            creaturechoice = r.choice(list(creatures))
+            print ("{} is now extinct".format(creaturechoice))
+            del creatures[creaturechoice]
+        else:
+            creaturename = "Creature " + str(len(creatures) + 1)
+            creatures[creaturename] = []
+            print ("{} has now been created".format(creaturename))
+            chance = 1
+            if chance == 1:
+                x = r.choice(list(creatures))
+                genesofcrature = list(creatures[x])
+                newgene = r.choice(genes)
+                genesofcrature.append(newgene)
+                t.sleep(1)
+                print ("{} has now have the {} gene".format(x, newgene))
+                creatures[x] = genesofcrature
+    t.sleep(1)
